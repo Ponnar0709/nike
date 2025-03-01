a0b8fac3a531cfc4653350e6c2c3814c5f180110
Ponnar0709 <ponnar007k@gmail.com>
2025-03-01 12:04:54 +0530

    'login'

--- src/Home.js
+++ src/Home.js
@@ -27,7 +27,7 @@ console.log(productData);
       <input type="text" placeholder="Search products..." className="searcha_bar" />
       
       <div className="links">
-        <p><a href="/home" className='l'> Home30</a></p>
+        <p><a href="/home" className='l'> Home</a></p>
         <p><a href="#" className='m'>Contact</a></p>
         <p><a href="#" className='p'>items</a></p>
       </div>
